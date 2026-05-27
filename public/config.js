e05e14fd762ef91d41bde341c2d5ee421046e8c9
Alex Sprüngli <soportesuenos@gmail.com>
2026-05-27 09:52:19 -0400

    fix: agregar config.js con API key y actualizar caché SW v10

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-﻿window.KAME_API_URL = 'https://fdl-kame-api.onrender.com';
+window.KAME_API_URL = 'https://fdl-kame-api.onrender.com';
 window.KAME_API_KEY = 'bAXFaqgbG1gpLbq6aweAIMfv4hOoNgKAeIgTQT67vLo';
